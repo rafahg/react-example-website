17278726c5ec9c7896a7b98d87cba8bacd2dfecb
pauladarias <pauladarias@gmail.com>
2020-12-07 08:59:05 +0000

    Change cards labels

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,20 +5,20 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Mira lo que hacemos en clase!</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
               src='/react-example-website/images/img-9.jpg'
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
+              label='Ciencias'
               path='/services'
             />
             <CardItem
               src='/react-example-website/images/img-2.jpg'
               text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
+              label='Arte'
               path='/services'
             />
           </ul>
@@ -26,19 +26,19 @@ function Cards() {
             <CardItem
               src='/react-example-website/images/img-3.jpg'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
+              label='Teatro'
               path='/services'
             />
             <CardItem
               src='/react-example-website/images/img-4.jpg'
               text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
+              label='Inglés'
               path='/products'
             />
             <CardItem
               src='/react-example-website/images/img-8.jpg'
               text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
+              label='Lengua'
               path='/sign-up'
             />
           </ul>

e5312c5186fcf931ef4b38d2ba63085d05eb3259
rafahg <r.hgar78@gmail.com>
2020-12-15 17:24:07 +0000

    Recursos page finished in this version

--- src/components/pages/Recursos.js
+++ src/components/pages/Recursos.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../App.css';
 
-export default function Services() {
+export default function Recursos() {
   return (
     <div className="services">
       <p className="recursos">RECURSOS</p>
